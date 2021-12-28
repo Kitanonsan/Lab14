c4667ccdb668a6352195cb20f0c6b0655f850f51
Non <Kitanon_san@cmu.ac.th>
2021-12-28 16:54:25 +0700

    Lab14_1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,49 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int N,double y[]){
+    double sum = 0 ,avg ,n = N;
+    for(int i=0; i < N; i++){
+        sum += x[i];
+    }
+    avg = sum/n;
+    y[0] = avg;
+
+    double sd;
+    sum = 0;
+    for(int i=0; i < N; i++){
+        sum += pow(x[i]-avg,2);
+    }
+    sd = sqrt(sum/n);
+    y[1] = sd;
+
+    double geo,mp=1;
+    for(int i=0; i < N; i++){
+        mp *= x[i];
+    }
+    geo = pow(mp,1/n);
+    y[2] = geo;
+
+    double hm;
+    sum = 0;
+    for(int i=0; i < N; i++){
+        sum += 1/x[i];
+    }
+    hm = n/sum;
+    y[3] = hm;
+
+    double max=x[0],min=x[0];
+    for(int i =1; i <N; i++){
+        if(x[i] < min) min = x[i];
+        if(x[i] > max) max = x[i];
+    }
+    y[4] = max;
+    y[5] = min;
+
+}
+
+
+
+        
+
